0cfb306208ed178d1264974a98474c9a7c030c7f
Karsten Finlay <karstenfinlay@hotmail.com>
2021-03-08 11:44:04 +0000

    User's balance starts at £0

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,5 +1,11 @@
 class Bank
 
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
   def withdraw(money)
     "You withdrew £#{money}"
   end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,6 +2,10 @@ require "bank"
 
 describe Bank do
 
+  it "checks user's balance at £0" do
+    expect(subject.balance).to eq 0
+  end
+
   describe "#withdraw" do
     it 'withdraws £10' do
       expect(subject.withdraw(10)).to eq "You withdrew £10"
